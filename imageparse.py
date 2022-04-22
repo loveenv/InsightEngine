28bf177604238f2d2db1b6192f45a1e41580ebef
Loveen V <loveenv@gmail.com>
2022-04-22 07:12:52 -0700

    trial

--- imageparse.py
+++ imageparse.py
@@ -1,9 +1,10 @@
 import pytesseract as tess
 from PIL import Image
-image = Image.open('pic.JPG')
-
-text = tess.image_to_string(image)
+img = Image.open('pic.JPG')
+##img.show()
+##text = tess.image_to_string(img)
+text = tess.image_to_string('pic.JPG')
 print(text)
-print('123abc')
-print(tess.__version__)
+##print('123abc')
+##print(tess.__version__)
 ##data = pytesseract.image_to_string(invert, lang='eng', config='--psm 6')
